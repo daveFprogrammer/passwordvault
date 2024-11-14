ddacdf2731650b5300efb67d47e4dec8f82a1372
Davide Faldi <davidefaldi@gmail.com>
2024-11-14 19:33:23 +0100

    Add User images for profile images and change model field

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    profile_image = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     two_factor_auth = models.BooleanField(default=False)
